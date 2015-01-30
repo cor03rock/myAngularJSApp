dda7e64167b2fda967262304e3529468f27ad105
Rocky Coronel <rocky.coronel@globalzeal.net>
2015-01-30 12:55:55 +0800

    Adding the 2nd part of the tutorial

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,1 +1,33 @@
-var app= angular.module ('myApp', []);
+var app = angular.module ('myApp', []);
+
+app.controller('SearchController', [ '$scope', function($scope){
+	$scope.master	;
+
+	$scope.search = function(songName){
+		$scope.master = angular.copy(songName);
+	};
+
+}]);
+
+app.controller('PlayerController', [ '$scope', function($scope){
+	$scope.playing = false;
+	$scope.audio = document.createElement('audio');
+	$scope.audio.src = "/resources/songs/Tremor.mp3";
+	$scope.play = function (){
+		$scope.audio.play()
+		$scope.playing = true;
+	};
+	$scope.stop = function (){
+		$scope.audio.pause();
+		$scope.playing = false;
+	};
+	$scope.audio.addEventListener( 'ended', function() {
+		$scope.$apply(function(){
+			$scope.stop()
+		});
+	});
+}]);
+
+app.controller('RelatedController', [ '$scope', function($scope){
+
+}]);
