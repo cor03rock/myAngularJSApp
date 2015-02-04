5717cb8b977279c2396ebfe05532eab1aa5380d0
Rocky Coronel <rocky.coronel@globalzeal.net>
2015-02-04 16:26:27 +0800

    Adding 3rd part of the training

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,6 @@
 var app = angular.module ('myApp', []);
+var apiKey = 'MDE4MTY0MjkyMDE0MjMwMzA1MDNjNDQyMQ001',
+	nprURL = 'http://api.npr.org/query?id=61&fields=relatedLink,title,text,audio&output=JSON';
 
 app.controller('SearchController', [ '$scope', function($scope){
 	$scope.master	;
@@ -9,25 +11,34 @@ app.controller('SearchController', [ '$scope', function($scope){
 
 }]);
 
-app.controller('PlayerController', [ '$scope', function($scope){
-	$scope.playing = false;
-	$scope.audio = document.createElement('audio');
-	$scope.audio.src = "/resources/songs/Tremor.mp3";
-	$scope.play = function (){
-		$scope.audio.play()
-		$scope.playing = true;
-	};
-	$scope.stop = function (){
-		$scope.audio.pause();
-		$scope.playing = false;
-	};
-	$scope.audio.addEventListener( 'ended', function() {
-		$scope.$apply(function(){
-			$scope.stop()
-		});
+// app.controller('PlayerController', [ '$scope', function($scope){
+// 	$scope.playing = false;
+// 	$scope.audio = document.createElement('audio');
+// 	$scope.audio.src = "/resources/songs/Tremor.mp3";
+// 	$scope.play = function (){
+// 		$scope.audio.play()
+// 		$scope.playing = true;
+// 	};
+// 	$scope.stop = function (){
+// 		$scope.audio.pause();
+// 		$scope.playing = false;
+// 	};
+// 	$scope.audio.addEventListener( 'ended', function() {
+// 		$scope.$apply(function(){
+// 			$scope.stop()
+// 		});
+// 	});
+// }]);
+app.controller('PlayerController', function($scope, $http){
+	$http({
+
+		method : 'JSONP',
+		url    : nprURL + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
+
+	}).success(function(data, status){
+		$scope.programs = data.list.story;
+	}).error(function(data, status){
+
 	});
-}]);
+});
 
-app.controller('RelatedController', [ '$scope', function($scope){
-
-}]);
